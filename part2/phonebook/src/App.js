486c548f573fe9647e009817253fcce85d8f2576
Román Larrosa <58295696+romanlarrosa@users.noreply.github.com>
2021-07-02 10:32:47 +0200

    Finished phonebook Part2 📞

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Filter = ({ onChange, searchValue }) => {
   return (
@@ -67,15 +68,16 @@ const Numbers = ({ persons, searchValue }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [searchValue, setSearchValue] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const nameOnChange = (event) => {
     const value = event.target.value
@@ -114,7 +116,6 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
       <Filter onChange={filterOnChange} searchValue={searchValue} />
 
       <h3>add a new</h3>
