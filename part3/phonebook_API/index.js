045fe353e83e1515904efb57e1d8cf2069964f65
Román Larrosa <58295696+romanlarrosa@users.noreply.github.com>
2021-07-06 11:10:58 +0200

    Exercises 3.5, 3.6 🔰

--- part3/phonebook_API/index.js
+++ part3/phonebook_API/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 const PORT = 3001
 
 let persons = [
@@ -32,7 +34,7 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
+  const person = persons.find((item) => item.id === id)
 
   if (person) {
     response.json(person)
@@ -46,6 +48,33 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  //Calculate an id (provisionally with "Math.random" )
+  const id = Math.ceil(Math.random() * 10000)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Content missing",
+    })
+  }
+
+  if (persons.find((item) => item.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id,
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 app.get("/info", (request, response) => {
